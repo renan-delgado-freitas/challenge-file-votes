d06bfc1ceafe7928f009b56b735f0f4a8d057580
renan-delgado-freitas <refreitas.ie@gmail.com>
2023-07-24 12:34:09 +0100

    codigo refatorado para ingles

--- src/in/read_file.py
+++ src/in/read_file.py
@@ -1,32 +1,29 @@
-import os
+def read_file(file="/home/renan/data/in/entrada.dat"):
+    data = []
+    with open(file, 'r') as file:
+        for line in file:
+            data.append(line.strip())
+        return data
 
 
-def ler_arquivos(arquivo='/home/renan/data/in/entrada.dat'):
-    dados = []
-    with open(arquivo, 'r') as arquivo:
-        for linha in arquivo:
-            dados.append(linha.strip())
-        return dados
+def filter_data(data):
+    salesmen = []
+    customers = []
+    sales = {}
 
-
-def filtrar_dados(dados):
-    clientes = []
-    vendedores = []
-    vendas = {}
-
-    for linha in dados:
-        item = linha.split('ç')
+    for line in data:
+        item = line.split('ç')
         if item[0] == '001':
-            clientes.append(item)
+            salesmen.append(item)
         elif item[0] == '002':
-            vendedores.append(item)
+            customers.append(item)
         elif item[0] == '003':
-            venda_id = item[1]
-            venda_items = item[2].replace('[', '').replace(']', '').split(',')
-            venda_total = sum(float(item.split('-')[1]) * float(item.split('-')[2]) for item in venda_items)
-            vendas[venda_id] = venda_total
-    return clientes, vendedores, vendas
+            sale_id = item[1]
+            sale_item = item[2].replace("[", "").replace("]", "").split(",")
+            total_sale = sum(float(item.split('-')[1]) * float(item.split('-')[2]) for item in sale_item)
+            sales[sale_id] = total_sale
+    return salesmen, customers, sales
 
 
 if __name__ == '__main__':
-    print(filtrar_dados(ler_arquivos()))
+    print(filter_data(read_file()))
