0a0d4fc2176a170010caa7ff13f68cc6aea29bcc
renan-delgado-freitas <refreitas.ie@gmail.com>
2023-07-24 21:09:42 +0100

    criada funcao de gerar relatorios

--- src/in/read_file.py
+++ src/in/read_file.py
@@ -19,11 +19,34 @@ def filter_data(data):
             customers.append(item)
         elif item[0] == '003':
             sale_id = item[1]
+            name = item[-1]
             sale_item = item[2].replace("[", "").replace("]", "").split(",")
             total_sale = sum(float(item.split('-')[1]) * float(item.split('-')[2]) for item in sale_item)
-            sales[sale_id] = total_sale
+            sales[sale_id] = total_sale, name
+
     return salesmen, customers, sales
 
 
+def create_report(salesmen, customers, sales):
+    w_salesman = sales.get(min(sales, key=sales.get))[1]
+
+    with open("/home/renan/PycharmProjects/challenge-file-votes/src/out/report.done.dat", 'w') as file:
+        file.write(f'Quantidade de vendedor no arquivo de entrada: {len(salesmen)}\n')
+        file.write(f'Quantidade de clientes no arquivo de entrada: {len(customers)}\n')
+        file.write(f'ID da venda mais cara: {max(sales, key=sales.get)}\n')
+        file.write(f'O pior vendedor: {w_salesman}\n')
+
+
+# Funcao criada apenas para testar o retorno do codigo
+def sales_per_salesman(sales):
+    for person in sales:
+        print(person, ": ", sales[person])
+
+
 if __name__ == '__main__':
-    print(filter_data(read_file()))
+    create_report(*filter_data(read_file()))
+    print(sales_per_salesman(filter_data(read_file())[2]))
+
+
+
+
