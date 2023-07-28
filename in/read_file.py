7489142d98126dc8bbb80ce04841f713494ae15a
renan-delgado-freitas <refreitas.ie@gmail.com>
2023-07-28 21:39:05 +0100

    criado loop para rodar o projeto continuamente

--- in/read_file.py
+++ in/read_file.py
@@ -1,4 +1,7 @@
-def read_file(file="/home/renan/data/in/entrada.dat"):
+import asyncio
+
+
+async def read_file(file="/home/renan/data/in/entrada.dat"):
     data = []
     with open(file, 'r') as file:
         for line in file:
@@ -6,15 +9,15 @@ def read_file(file="/home/renan/data/in/entrada.dat"):
         return data
 
 
-def filter_data(data):
-    salesmen = []
+async def filter_data(data):
+    sellers = []
     customers = []
     sales = {}
 
     for line in data:
         item = line.split('ç')
         if item[0] == '001':
-            salesmen.append(item)
+            sellers.append(item)
         elif item[0] == '002':
             customers.append(item)
         elif item[0] == '003':
@@ -24,29 +27,37 @@ def filter_data(data):
             total_sale = sum(float(item.split('-')[1]) * float(item.split('-')[2]) for item in sale_item)
             sales[sale_id] = total_sale, name
 
-    return salesmen, customers, sales
+    return sellers, customers, sales
 
 
-def create_report(salesmen, customers, sales):
-    worst_salesman = sales.get(min(sales, key=sales.get))[1]
+async def create_report(seller, customers, sales):
+    worst_seller = sales.get(min(sales, key=sales.get))[1]
 
     with open("/home/renan/PycharmProjects/challenge-file-votes/out/report.done.dat", 'w') as file:
-        file.write(f'Quantidade de vendedor no arquivo de entrada: {len(salesmen)}\n')
+        file.write(f'Quantidade de vendedor no arquivo de entrada: {len(seller)}\n')
         file.write(f'Quantidade de clientes no arquivo de entrada: {len(customers)}\n')
         file.write(f'ID da venda mais cara: {max(sales, key=sales.get)}\n')
-        file.write(f'O pior vendedor: {worst_salesman}\n')
+        file.write(f'O pior vendedor: {worst_seller}\n')
 
 
 # Funcao criada apenas para testar o retorno do codigo
-def sales_per_salesman(sales):
-    for person in sales:
-        print(person, ": ", sales[person])
+async def sales_per_salesman(sales):
+    for seller in sales:
+        print(seller, ": ", sales[seller])
+
+
+async def main():
+    while True:
+        data = await read_file()
+        sellers, customers, sales = await filter_data(data)
+        await create_report(sellers, customers, sales)
+        await sales_per_salesman(sales)
+        print(35 * '-')
+        await asyncio.sleep(3)
 
 
 if __name__ == '__main__':
-    create_report(*filter_data(read_file()))
-    print(sales_per_salesman(filter_data(read_file())[2]))
-
+    asyncio.run(main())
 
 
 
